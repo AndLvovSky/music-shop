c6c9602e32be24243f4eaceecd0aec161033bc5a
AndLvovSky <viktorilvovskyi@ukr.net>
2019-04-24 14:07:19 +0300

    Add slider

---
+++ scripts.js
@@ -0,0 +1,38 @@
+var sliderIndex = -1;
+
+function init() {
+	onSliderChange();	
+}
+
+function onSliderChange() {
+	var sliderImages = document.getElementsByClassName("slider-image");
+	if (sliderIndex == -1) {
+		var target = sliderImages[0];
+		target.style.display = "block"; 
+		target.classList.add("unfade-image");
+		setTimeout(function() {
+			target.classList.remove("unfade-image");
+			sliderIndex = 0;
+			onSliderChange();
+		}, 2000);
+		return;
+	}  
+	for (var sliderImage of sliderImages) {
+		sliderImage.style.display = "none";
+	}
+	var previous = sliderImages[sliderIndex];
+	sliderIndex = (sliderIndex + 1) % sliderImages.length;
+	var target = sliderImages[sliderIndex]; 
+	previous.classList.add("fade-image");
+	previous.style.display = "block";
+	setTimeout(function() {
+		previous.style.display = "none";
+		previous.classList.remove("fade-image");
+		target.classList.add("unfade-image");
+		target.style.display = "block";
+	}, 2000);
+	setTimeout(function() {
+		target.classList.remove("unfade-image");
+		onSliderChange();
+	}, 4000);
+}
