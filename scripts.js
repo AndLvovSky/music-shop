da307bc21a4ef0edd26a6a64d23a14d05ef7a507
AndLvovSky <viktorilvovskyi@ukr.net>
2019-05-16 00:17:17 +0300

    Restyle footer

--- scripts.js
+++ scripts.js
@@ -24,7 +24,7 @@ function onSliderChange() {
 		$(".slider-image").eq(0)
 			.css("display", "block")
 			.animate({opacity: 1}, 2000, () => {
-				onSliderChange();
+				//onSliderChange();
 			});
 		return;
 	}
